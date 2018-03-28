99edd2b7bd482162bb2597c6e691ace9335eb49f
Max <maxmal86@gmail.com>
2018-03-27 22:31:39 -0500

    Store and Components connected with React-Redux, rendering individual expenses and controlled inputs for filters completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
+const store = configureStore();
+
+store.subscribe(() => {
+	const state = store.getState();
+	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+	console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: "Water Bill", amount: 70 }));
+store.dispatch(addExpense({ description: "Gas Bill", amount: 60, createdAt: 1000 }));
+store.dispatch(addExpense({ description: "rent", amount: 1145 }));
+
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
+);
 
 
 
-ReactDOM.render(<AppRouter />, document.querySelector("#app"));
+
+ReactDOM.render(jsx, document.querySelector("#app"));
 
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFilters";
 
 const ExpenseDashboardPage = () => (
 	<div>
-		This is from my dashboard component
+		<ExpenseListFilters />
+		<ExpenseList />
 	</div>
 );
 
