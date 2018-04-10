4170068e3d222751b1fa4dd716e574422b41b8a2
Max <maxmal86@gmail.com>
2018-04-09 21:14:16 -0500

    Production Webpack config completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 const path = require("path");
 
-module.exports = {
-	entry: "./src/app.js",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [{
-			loader: "babel-loader",
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				"style-loader",
-				"css-loader",
-				"sass-loader"
-			]
-		}]
-	},
-	devtool: "cheap-module-eval-source-map", // for mapping errors in the console
-	devServer: {
-		contentBase: path.join(__dirname, "public"),
-		historyApiFallback: true // for client-side routing, 404 will fallback to index.html
+module.exports = (env) => {
+	
+	const isProduction = env === "production";
+
+	return {
+		entry: "./src/app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js"
+		},
+		module: {
+			rules: [{
+				loader: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				]
+			}]
+		},
+		devtool: isProduction ? "source-map" : "cheap-module-eval-source-map", // for mapping errors in the console
+		devServer: {
+			contentBase: path.join(__dirname, "public"),
+			historyApiFallback: true // for client-side routing, 404 will fallback to index.html
+		}
 	}
 };
+
