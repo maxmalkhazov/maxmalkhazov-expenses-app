60cb945fee49c201d664a10a6bfad72d4e7b2bbc
Max <maxmal86@gmail.com>
2018-04-28 19:30:15 -0500

    Auth reducer completed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from "./actions/auth";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -38,6 +38,8 @@ ReactDOM.render(<p>Loading...</p>, document.querySelector("#app"));
 
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
+		console.log("uid", user.uid);
+		store.dispatch(login(user.uid));
 		store.dispatch(startSetExpenses()).then(() => {
 			renderApp();
 			if (history.location.pathname === "/") {
@@ -45,6 +47,7 @@ firebase.auth().onAuthStateChanged((user) => {
 			}
 		});
 	} else {
+		store.dispatch(logout());
 		renderApp();
 		history.push("/");
 	}
